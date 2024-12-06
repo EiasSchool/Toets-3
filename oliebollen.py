bbe7fede6780698d54c8ef60d1a7d5975c8d9ae1
Eias <51035404+1Eias@users.noreply.github.com>
2024-12-06 11:22:20 +0100

    T3.O3

--- oliebollen.py
+++ oliebollen.py
@@ -1,3 +1,6 @@
+print("-------- ACTIE --------")
+print("ELKE 10 OLIEBOLLEN KOSTEN €10.00 INPLAATS VAN €11.50\n")
+
 while True:
     try:
         oliebollen = int(input("Hoeveel oliebollen (van €1,15) wilt u bestellen? "))
@@ -20,16 +23,22 @@ while True:
 
 oliebollenPrijs = 1.15
 appelflappenPrijs = 1.60
+zakprijs = 10.00
 
-totaalOliebollen = oliebollen * oliebollenPrijs
+aantalZakken = oliebollen // 10
+loseoliebollen = oliebollen - (aantalZakken * 10)
+
+totaalOliebollen = (aantalZakken * 10 * 1) + (loseoliebollen * oliebollenPrijs)
 totaalAppelflappen = appelflappen * appelflappenPrijs
 
 totaal = totaalOliebollen + totaalAppelflappen
 
-print("------ [UW BESTELLING] ------")
+print("-------- [UW BESTELLING] --------")
 if oliebollen > 0:
-    print(f"Oliebol: {oliebollen:>2} x €{oliebollenPrijs:<4.2f} = €{totaalOliebollen:>5.2f}")
+    print(f"Oliebol (zak): {aantalZakken:>2} x €{zakprijs:<5.2f} = €{aantalZakken * 10 * 1:>7.2f}")
+    if loseoliebollen > 0:
+        print(f"Oliebol (los): {loseoliebollen:>2} x €{oliebollenPrijs:<5.2f} = €{loseoliebollen * oliebollenPrijs:>7.2f}")
 if appelflappen > 0:
-    print(f"Appelflap:{appelflappen:>2} x €{appelflappenPrijs:<4.2f} = €{totaalAppelflappen:>5.2f}")
-print("-----------------------------+")
-print(f"Totaal:                 {totaal:5.2f}")
+    print(f"Appelflap:     {appelflappen:>2} x €{appelflappenPrijs:<5.2f} = €{totaalAppelflappen:>7.2f}")
+print("---------------------------------+")
+print(f"Totaal:                     {totaal:5.2f}")
